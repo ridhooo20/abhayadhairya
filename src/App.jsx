9b417dbbabd49785e0216d00bd35ec716099fece
Ridho Mulya <githubridho@contoh.com>
2024-12-16 01:26:35 +0700

    deploy react to github

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import "./style.css";
 import Navbar from './Component/Navbar';
 import Hero from './Component/Hero';
 import Home from './Component/Home';
-import Album from './Component/Album'
-import Footer from './Component/Footer'
-import Audio from './Component/Audio'
+import Album from './Component/Album';
+import Footer from './Component/Footer';
+import Audio from './Component/Audio';
+import "./style.css";
+
 const App = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:p-0 p-0">
@@ -13,7 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <Home />
-        <Album/>
+        <Album />
         <Footer />
         <Audio />
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './Abhaya.jsx'
-import erorPage from "./404/404.jsx"
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Login from './Component/Login'
-import Dashboard from './Component/Dashboard'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './Component/Login';
+import Dashboard from './Component/Dashboard';
+
 const router = createBrowserRouter([
   {
-    path: "/Abhaya",
-    element: <App/>
+    path: "/",
+    element: <App />,
   },
-
-  {
-    path:"/",
-    errorElement: <erorPage/>,
-  },
-
   {
     path: "/Loginabhaya",
-    element: <Login/>
+    element: <Login />,
   },
-
   {
     path: "/Dashboard",
-    element: <Dashboard/>
-  }
-
+    element: <Dashboard />,
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
